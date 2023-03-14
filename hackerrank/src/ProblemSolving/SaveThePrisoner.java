fe645bd2f23407108ccd7d15cdc8181fc9f22a35
Samuel Akuma <usakuma@uwaterloo.ca>
2023-03-13 23:29:20 -0400

    SaveThePrisoner.java solution
    https://www.hackerrank.com/challenges/save-the-prisoner/problem

---
+++ hackerrank/src/ProblemSolving/SaveThePrisoner.java
@@ -0,0 +1,57 @@
+package ProblemSolving;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class ResultSaveThePrisoner {
+
+    /*
+     * Complete the 'saveThePrisoner' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     *  3. INTEGER s
+     */
+
+    public static int saveThePrisoner(int n, int m, int s) {
+        // Write your code here
+
+        int result =  (m  + s - 1) % n;
+        return result == 0 ? n : result;
+    }
+
+}
+
+public class SaveThePrisoner {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int s = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = ResultSaveThePrisoner.saveThePrisoner(n, m, s);
+                System.out.println(result);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
